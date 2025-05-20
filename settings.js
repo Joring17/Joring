4f6db518f036f9daa6dcf1120c1ad4ccd7980d9f
Joring <142078739+Joring17@users.noreply.github.com>
2025-05-20 14:44:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Big Black Gorilla",
   botOwner: 'Joring', // Your name
   ownerNumber: '916900986342', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
